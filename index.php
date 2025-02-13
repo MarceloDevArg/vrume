505f66a11b77c61b96a175095d377c8209b82272
MarceloDevArg <sponsorfull@gmail.com>
2025-02-12 21:07:41 -0300

    Adding pipedrive forms

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="robots" content="noindex, nofollow" />
+		<title>Vrume</title>
+        <meta name="viewport" content="user-scalable=false, initial-scale=1.0, maximum-scale=1.0">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+		<meta name="theme-color" content="#000000">
+		<link rel="stylesheet" href="https://use.typekit.net/sme3lqw.css">
+        <link rel="stylesheet" type="text/css" href="css/slick.css">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+
+	<header>
+        <video preload="none" autoplay loop playsinline muted src="media/107464-678258863_small.mp4"></video>
+
+		<div class="header__content">
+
+            <div class="header__top">
+                <img src="images/vrume-logo-white.png" alt="">
+
+                <input id="menu-toggle" type="checkbox" />
+                <label class='menu-button-container' for="menu-toggle"><div class='menu-button'></div></label>
+
+                <ul class="nav">
+                    <li><a href="">Home</a></li>
+                    <li><a href="advertisers.html">Advertisers</a></li>
+                    <li><a href="">Publisher</a></li>
+                    <!--
+                    <li><a href="">Design</a></li>
+                    <li><a href="">Case Studies</a></li>
+                    <li><a href="">Domains for sale</a></li>
+                    -->
+                    <li><a href="">Contact us</a></li>
+                </ul>
+            </div>
+
+			<div class="header__copy">
+                <h1>WE Drive Traffic and Sales!</h1>
+			    <h2>Whether you’re a seasoned marketer or just starting to scale your business,
+                    Vrume.com specializes in customized online advertising strategies that accelerate
+                    your growth. We focus on driving high-quality traffic and targeted customers
+                    straight to your offers, with expertise in male-focused verticals like dating,
+                    gaming, male enhancement, and more. Let us help you achieve measurable success
+                    with campaigns designed for impact.</h2>
+
+                <div class="header__btn">
+                    <a href="" class="cta">BECOME AN ADVERTISER</a>
+                    <a href="" class="cta active">BECOME AN PUBLISHER</a>
+                </div>
+            </div>
+		</div>
+	</header>
+
+    <section class="wwd">
+        <div class="wwd__wrapper">
+
+            <div class="wwd__copy">
+                <h1>Engage, Convert, and Scale 
+                    Your Business with Precision</h1>
+			    <h2>At Vrume, we don’t just sell traffic—we help you dominate your market. Our
+                    expert team connects you with high-quality, targeted traffic sources that
+                    turns clicks into paying customers and campaigns into profits.
+                </h2>
+            </div>
+
+        </div>
+
+        <div class="wwd_slider">
+            <div>
+                <h1>DRIVE ENGAGEMENT and MAXIMIZE IMPACT</h1>
+                <p>At Vrume.com, our advanced targeting capabilities ensure your campaigns deliver the right message to the right audience at the perfect time. From email and push notifications to banner ads and landing page traffic, we connect you with high-intent users ready to engage.</p>
+            </div>
+            <div>
+                <h1>CPC & CPM PERFORMANCE MARKETING</h1>
+                <p>Our performance-based marketing models empower you to only pay for what works. Whether it’s CPC (Cost Per Click) or CPM (Cost Per Thousand Impressions), our approach ensures you’re investing in measurable results and maximizing your advertising ROI.</p>
+            </div>
+            <div>
+                <h1>REACH YOUR IDEAL AUDIENCE WITH VRUME.COM</h1>
+                <p>At Vrume.com, we ensure your message connects with the right people at the right time. Through advanced targeting and high-performing ad placements like email, push, banner, and landing page traffic, we help you cut through the noise.</p>
+            </div>
+            <div>
+                <h1>REACH YOUR IDEAL AUDIENCE WITH VRUME.COM</h1>
+                <p>At Vrume.com, we ensure your message connects with the right people at the right time. Through advanced targeting and high-performing ad placements like email, push, banner, and landing page traffic, we help you cut through the noise.</p>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="hiw">
+        <div class="hiw__wrapper">
+
+            <div class="hiw__copy">
+                <h1>OUR PROCESS IS SIMPLE, EFFECTIVE,
+                    AND RESULTS-DRIVEN
+                </h1>
+
+                <div class="hiw__items">
+                    <div class="hiw__items-item">
+                        <div class="item-number">1</div>
+                        <div class="item-copy">
+                            <h1>Pinpoint Your Ideal Traffic</h1>
+                            <p>At Vrume.com, we use advanced targeting strategies to match your campaigns with the perfect traffic. Whether you’re aiming for email, push notifications, banners, or premium placements, we ensure your efforts align with the right users to maximize ROI.</p>
+                        </div>
+                    </div>
+
+                    <div class="hiw__items-item">
+                        <div class="item-number">2</div>
+                        <div class="item-copy">
+                            <h1>Optimize for Performance</h1>
+                            <p>We continuously monitor and analyze your campaigns to refine performance and boost results. With our focus on conversions and ROI, we ensure every advertising dollar is spent wisely to generate measurable success.
+
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="hiw__items-item">
+                        <div class="item-number">3</div>
+                        <div class="item-copy">
+                            <h1>Tailor Your Campaigns</h1>
+                            <p>From custom creatives to precise ad placements, we design campaigns that resonate with your target customers. Our expertise ensures every click or impression is strategically positioned for impact and relevance.</p>
+                        </div>
+                    </div>
+
+                    <div class="hiw__items-item">
+                        <div class="item-number">4</div>
+                        <div class="item-copy">
+                            <h1>Scale Your Growth</h1>
+                            <p>Once we’ve dialed in your campaign’s success, we help you scale efficiently. Whether expanding your CPC campaigns, introducing CPM strategies, or exploring new traffic channels, Vrume.com is here to grow with you sustainably.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <img src="images/element-bg-waves.png" class="element-waves">
+    </section>
+
+    <section class="tes">
+        <div class="tes__wrapper">
+            <div class="tes__copy">
+                <h1>What Our Customers Have to Say</h1>
+            </div>
+        </div>
+
+        <div class="tes_slider">
+            <div>
+                <p>I have worked with Mark and his team from Vrume since 2005. They know how to drive quality traffic and work with their buyers to get results.</p>
+                <h1>Daniel / <span>Adworldmedia</span></h1>
+                <img src="images/adworld-logo.png" alt="adworld">
+            </div>
+            <div>
+                <p>I have worked with Vrume for 2+ years and it’s been a great experience. He provides high-quality traffic for multiple geos, is easy to work with, and always delivers on time.</p>
+                <h1>MARINUS / <span>Flirtynlocal</span></h1>
+                <img src="images/flirtynloca-logo.png" alt="flirtynloca">
+            </div>
+            <div>
+                <p>I´ve been working with Mark and Oliver for a while and I like their personal approach in always finding the best spots. They are very professional, responsive and easy to work with. I can recommend them to anybody looking for quality traffic</p>
+                <h1>Fabian Jansen / <span>Adthorized</span></h1>
+                <img src="images/adthorized-logo.png" alt="adthorized">
+            </div>
+            <div>
+                <p>I've been working with Vrume for quite some time now. It's been a blast so far! Strong collaboration, fast communication and all round good guys! Looking forward to see what the future will bring.</p>
+                <h1>Jarno / <span>REBLL NETWORK</span></h1>
+                <img src="images/rebll-logo.png" alt="rebll">
+            </div>
+        </div>
+
+        <div class="counter__wrapper">
+            <div class="tes__counter">
+                <div class="counter__item">
+                    <span class="counter__item-number">2.5m+</span>
+                    <h3>conversions daily</h3>
+                </div>
+                <div class="counter__item">
+                    <span class="counter__item-number">1.4K+</span>
+                    <h3>active partners</h3>
+                </div>
+                <div class="counter__item">
+                    <span class="counter__item-number">25+</span>
+                    <h3>years of expertise in affiliate marketing</h3>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <footer>
+        <div class="footer__wrapper">
+            <div class="hiw__copy">
+                <h1>Have a question?<br /> 
+                    Feel free to contact us.                    
+                </h1>
+            </div>
+
+            <div class="footer__content">
+
+                <div class="pipedriveWebForms" data-pd-webforms="https://webforms.pipedrive.com/f/30XwHR3yZByfby67E9hb6jgpf4lqMRMxWYRB2s1l5TTooqhcHaSSHWpoW3GraLl1p"><script src="https://webforms.pipedrive.com/f/loader"></script></div>
+
+                <div class="footer__social">
+                    <h3>Social</h3>
+                    <ul>
+                        <li><a href=""><img src="images/fb.png" alt=""></a></li>
+                        <li><a href=""><img src="images/tw.png" alt=""></a></li>
+                        <li><a href=""><img src="images/ig.png" alt=""></a></li>
+                        <li><a href=""><img src="images/in.png" alt=""></a></li>
+                    </ul>
+                    <p>
+                        Platform @ The Junction.<br />
+                        5 Edinburgh Rd S Guelph, Ont N1H 5N8,<br />
+                        Canada<br />
+                        <br />
+                        Copyright © 2024 Vrume. All rights reserved.
+                    </p>
+                </div>
+
+            </div>
+
+        </div>
+
+        <img src="images/element-bg-waves.png" class="element-waves">
+    </footer>
+
+	
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/slick.min.js"></script>
+<script>
+	$('.wwd_slider,.tes_slider').slick({
+        centerMode: true,
+		slidesToShow: 3,
+        slidesToScroll: 1,
+		infinite: true,
+		draggable: false,
+		speed: 1500,
+		dots: true,
+		arrows: false,
+        autoplay: true,
+		responsive: [
+		{
+		  breakpoint: 980,
+		  settings: {
+			slidesToShow: 2
+		  }
+		},
+        {
+		  breakpoint: 720,
+		  settings: {
+			slidesToShow: 1
+		  }
+		}
+	  ]
+	});
+
+    $(window).scroll(function() {
+        var top_of_element = $(".hiw").offset().top;
+        var bottom_of_element = $(".hiw").offset().top + $(".hiw").outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+            $('.wwd').addClass('inviewport')
+        } else {
+            $('.wwd').removeClass('inviewport')
+        }
+    });
+</script>
+</body>
+</html>
